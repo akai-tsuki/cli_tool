6f4c9c38bc47c6e0d97a8c458a4a59a942b0db87
akai-tsuki <akai.tsuki.mobile@gmail.com>
2020-03-22 02:30:12 +0900

    Add test pattern

--- tests/test_rest_client.py
+++ tests/test_rest_client.py
@@ -1,21 +1,40 @@
 import unittest
 from cli_tool.rest_client import RestClient
+from unittest.mock import Mock, patch
 
 
 class TestRestClient(unittest.TestCase):
     """test class of RestClient
     """
 
-    def test_get_weather(self):
-        """test method for get_msg
+    @patch("cli_tool.rest_client.requests")
+    def test_get_weather(self, mock_requests):
+        """test method for get_weather
         """
 
+        mock_response = Mock(status_code=201, url="test_url", text="test_text")
+        mock_requests.get.return_value = mock_response
+
         # prepare
-        expected = 200
+        expected = 201
 
         # execute
         client = RestClient()
         actual = client.get_weather('140010')
+
+        # check
+        self.assertEqual(expected, actual)
+
+    def test_get_greeting(self):
+        """test method for get_greeting
+        """
+
+        # prepare
+        expected = "Hello test name!"
+
+        # execute
+        client = RestClient()
+        actual = client.get_greeting("test name")
 
         # check
         self.assertEqual(expected, actual)
