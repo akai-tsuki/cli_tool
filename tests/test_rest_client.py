bbed18d67ad926e8918e24fbf97e780a97aa5b8d
akai-tsuki <akai.tsuki.mobile@gmail.com>
2020-03-22 17:34:42 +0900

    Add test case using mock

--- tests/test_rest_client.py
+++ tests/test_rest_client.py
@@ -3,6 +3,26 @@ from cli_tool.rest_client import RestClient
 from unittest.mock import Mock, patch
 
 
+# requests lib の mock
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+            self.url = "mocked_url"
+            self.text = "mocked_text"
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == "http://localhost:18080/greeting":
+        return MockResponse({"id": 1, "content": "Hello test name!"}, 200)
+    elif args[0] == "http://weather.livedoor.com/forecast/webservice/json/v1":
+        return MockResponse({}, 200)
+    else:
+        return MockResponse({}, 404)
+
+
 class TestRestClient(unittest.TestCase):
     """test class of RestClient
     """
@@ -12,10 +32,10 @@ class TestRestClient(unittest.TestCase):
         """test method for get_weather
         """
 
+        # prepare
         mock_response = Mock(status_code=201, url="test_url", text="test_text")
         mock_requests.get.return_value = mock_response
 
-        # prepare
         expected = 201
 
         # execute
@@ -25,7 +45,23 @@ class TestRestClient(unittest.TestCase):
         # check
         self.assertEqual(expected, actual)
 
-    def test_get_greeting(self):
+    @patch('requests.get', side_effect=mocked_requests_get)
+    def test_get_weather2(self, mock_requests_get):
+        """test method for get_weather2
+        """
+
+        # prepare
+        expected = 200
+
+        # execute
+        client = RestClient()
+        actual = client.get_weather('140010')
+
+        # check
+        self.assertEqual(expected, actual)
+
+    @patch('requests.get', side_effect=mocked_requests_get)
+    def test_get_greeting(self, mock_requests_get):
         """test method for get_greeting
         """
 
